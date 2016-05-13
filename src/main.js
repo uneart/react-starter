686c7625905da96a08547350e0e1532626c2d407
Sascha Möser <sascha@uneart.de>
2016-05-13 14:20:12 +0200

    Add basic Redux folder structure

--- src/main.js
+++ src/main.js
@@ -1,21 +1,23 @@
-import React, { Component }  from 'react';
+import React  from 'react';
 import ReactDOM  from 'react-dom';
+import { Provider }  from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+
 import { Router, Route } from 'react-router';
 import { createHistory } from 'history';
 
+import reducers from './reducers'
 
-// import StorePicker from './components/StorePicker';
 import App from './components/App';
 import NotFound from './components/NotFound';
 
-/*
-  Routes
-*/
-var routes = (
-  <Router history={createHistory()}>
-    <Route path="/" component={App}/>
-    <Route path="*" component={NotFound}/>
-  </Router>
-)
+const createStoreWithMiddleware = applyMiddleware()(createStore);
 
-ReactDOM.render(routes, document.querySelector('#main'));
+ReactDOM.render(
+    <Provider store={createStoreWithMiddleware(reducers)}>
+      <Router history={createHistory()}>
+        <Route path="/" component={App}/>
+        <Route path="*" component={NotFound}/>
+      </Router>
+    </Provider>
+    , document.querySelector('#main'));
